34dc61d31557be94147c101116543e20f7a23387
Max Rose <max.rose366@gmail.com>
2025-10-05 20:19:30 +0100

    home on nav click

--- Client/src/components/Navbar.vue
+++ Client/src/components/Navbar.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
 import ThemeSelector from '@/components/ThemeSelector.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <template>
   <nav class="flex w-screen p-2 items-center justify-center">
     <div class="flex items-center justify-between w-full max-w-2/3">
-      <span class="flex flex-row items-center justify-center gap-2">
+      <span
+        class="select-none cursor-pointer flex flex-row items-center justify-center gap-2"
+        @click="router.push('/')"
+      >
         <img alt="Planning Poker Logo" class="h-10 mr-2" src="@/assets/logo.svg" />
         <h1 class="scroll-m-20 text-4xl tracking-tight lg:text-5xl">Planning Poker</h1>
       </span>

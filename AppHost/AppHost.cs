e96f6a3e207aeab1ffcf0621e88a161f045e859f
Max Rose <max.rose366@gmail.com>
2025-11-13 10:52:00 +0000

    feat: Update to .NET 10

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -6,9 +6,9 @@ var api = builder.AddProject<Api>("Api")
     .WithEnvironment("Serilog__WriteTo__0__Args__Configure__0__Args__theme", "Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme::Sixteen, Serilog.Sinks.Console")
     .WithEnvironment("Serilog__WriteTo__0__Args__Configure__0__Args__applyThemeToRedirectedOutput", "true");
 
-builder.AddViteApp("Client", Path.Join(builder.AppHostDirectory, "..", "Client"), "pnpm")
-    .WithPnpmPackageInstallation()
-    .WaitFor(api)
-    .WithEnvironment("VITE_BACKEND_API_URL", api.GetEndpoint("http"));
+builder.AddViteApp("Client", Path.Join(builder.AppHostDirectory, "..", "Client"))
+    .WithPnpm()
+    .WithEnvironment("VITE_BACKEND_API_URL", api.GetEndpoint("https"))
+    .WaitFor(api);
 
 builder.Build().Run();
